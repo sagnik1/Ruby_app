654efb04d443b3df3ff4873ced1defd89313d19a
sagnik1 <sagnikProj@outlook.com>
2016-01-23 21:58:22 +0530

    Finish user signing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
   get 'signup'  => 'users#new'
+
+  resources :users
+  
 end
